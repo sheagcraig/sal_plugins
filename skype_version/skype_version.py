71b8dfbb909d964f457e04075db24ab57d286fdf
root <root@macreporting.na.sas.com>
2018-04-17 09:00:53 -0400

    Update plugins to new-style.

--- skype_version/skype_version.py
+++ skype_version/skype_version.py
@@ -1,57 +1,25 @@
-#!/usr/bin/python
-
-
 from django.db.models import Count
-from django.shortcuts import get_object_or_404
-from django.template import loader, Context
-from yapsy.IPlugin import IPlugin
-from yapsy.PluginManager import PluginManager
 
+import sal.plugin
 from inventory.models import InventoryItem
-from server.models import *
-import server.utils as utils
 
 
-class SkypeVersion(IPlugin):
-    name = "SkypeVersion"
+class SkypeVersion(sal.plugin.Widget):
 
-    def widget_width(self):
-        return 4
+    description = "Skype for Business Version"
 
-    def plugin_type(self):
-        return "builtin"
+    def get_context(self, queryset, **kwargs):
+        context = self.super_get_context(queryset, **kwargs)
+        context['data'] = (
+             InventoryItem.objects.filter(
+                 machine__in=queryset,
+                 application__name="Skype for Business",
+                 application__bundleid__startswith="com.microsoft") 
+             .values("version")
+             .annotate(count=Count("version"))
+             .order_by("version"))
 
-    def get_description(self):
-        return "Skype for Business Version"
-
-    def widget_content(self, page, machines=None, theid=None):
-        # The data is data is pulled from the database and passed to a template.
-
-        # There are three possible views we're going to be rendering to -
-        # front, bu_dashboard and group_dashboard. If page is set to
-        # bu_dashboard, or group_dashboard, you will be passed a business_unit
-        # or machine_group id to use (mainly for linking to the right search).
-        if page == "front":
-            t = loader.get_template(
-                "kvnjcby/skype_version/templates/front.html")
-        elif page in ("bu_dashboard", "group_dashboard"):
-            t = loader.get_template(
-                "kvnjcby/skype_version/templates/id.html")
-
-        data = InventoryItem.objects.filter(machine__in=machines,
-                                            application__name="Skype for Business",
-                                            application__bundleid__startswith="com.microsoft") \
-                                    .values("version") \
-                                    .annotate(count=Count("version")) \
-                                    .order_by("version")
-
-        c = {
-            "title": self.get_description(),
-            "data": data,
-            "theid": theid,
-            "page": page}
-
-        return t.render(c)
+        return context
 
     def filter_machines(self, machines, data):
         machines = machines.filter(inventoryitem__application__name="Skype for Business",
